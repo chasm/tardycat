2dc6d20cfe73d8f63a31c129178b290ba9d3019f
chasm <charles.munat@gmail.com>
2014-03-09 22:57:17 +0800

    Added authentication methods.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.2'
+gem 'rails', '4.0.3'
 gem 'activesupport', require: 'active_support'
 gem 'uglifier'
 gem 'jquery-rails'
@@ -8,6 +8,7 @@ gem 'jquery-ui-rails'
 gem 'slim'
 gem 'bson_ext'
 gem 'mongoid', github: 'mongoid/mongoid'
+gem 'bcrypt'
 
 group :development, :test do
   gem "better_errors"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
+require 'bcrypt'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
+  before_save :encrypt_password
+
   field :email, type: String
+  field :salt, type: String
+  field :fish, type: String
+
+  def authenticate(password)
+    self.fish == BCrypt::Engine.hash_secret(password, self.salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by email: email
+    user if user and user.authenticate(password)
+  end
+
+  protected
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+    end
+  end
 end
