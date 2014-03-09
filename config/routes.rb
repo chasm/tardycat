96d29a96f15db5a8a30553ea047762dfcb707000
chasm <charles.munat@gmail.com>
2014-03-09 23:35:11 +0800

    Added authentication routes and actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 P2::Application.routes.draw do
+  get    'login'  => 'session#new'
+  post   'login'  => 'session#create'
+  delete 'logout' => 'session#destroy'
+  get    'logout' => 'session#destroy' # TODO: remove before deployment
+
   get 'privacy' => 'site#privacy'
   get 'terms'   => 'site#terms'
 
