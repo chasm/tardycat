5ed4e85e03211816fa12a2f1e21dc44a9cc04208
chasm <charles.munat@gmail.com>
2014-03-10 17:46:52 +0800

    Updated the seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 User.destroy_all
 
+temp_password = "123"
+
 User.create([
-  { email: "joe@example.com" },
-  { email: "betty@example.com" },
-  { email: "tim@example.com" }
+  {
+    email: "joe@example.com",
+    password: temp_password,
+    password_confirmation: temp_password
+  },
+  {
+    email: "betty@example.com",
+    password: temp_password,
+    password_confirmation: temp_password
+  },
+  {
+    email: "tim@example.com",
+    password: temp_password,
+    password_confirmation: temp_password
+  }
 ])
