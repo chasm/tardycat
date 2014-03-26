b5d7c7f0c361a46d520a2f1d4524d57b1ed53055
chasm <charles.munat@gmail.com>
2014-03-26 17:14:48 +0800

    Moved noty to vendor folder where it belongs. Added main.coffee and changes application.css to scss.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require ./vendor/noty/packaged/jquery.noty.packaged.min
-//= require ./vendor/noty/themes/default
+//= require noty/packaged/jquery.noty.packaged.min
+//= require noty/themes/default
+//= main
